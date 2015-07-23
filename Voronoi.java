de3618ecefd83c75728a1441fd6099ecdf517e5b
Ninespring <xiaolongshen.stuff@gmail.com>
2015-07-23 23:12:09 +0800

    Init Commit For Voronoi Algorithm Implementation.˚

---
+++ Voronoi.java
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 15/7/23.
+ * Author: Xiaolong Shen @ Nexd Tech
+ */
+
+/*
+    ToDo: Create Vornoi Algorithm
+    ToDo: Build Up Unit Test Samples For Distance Checking
+ */
+
+public class Voronoi {
+
+}
